10159df434c88d87357117895d42af1fa6b5ce68
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-07-29 04:16:59 -0700

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Performs insertion sort on a doubly linked list
+ * @list: Double pointer to linked list listint_t
+ *
+ * Return: Nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *tmp;
+	if (!list)
+		return;
+	ptr = *list;
+	while (ptr)
+	{
+		while (ptr->next && (ptr->n > ptr->next->n))
+		{
+			tmp = ptr->next;
+			ptr->next = tmp->next;
+			tmp->prev = ptr->prev;
+			if (ptr->prev)
+				ptr->prev->next = tmp;
+			if (tmp->next)
+				tmp->next->prev = ptr;
+			ptr->prev = tmp;
+			tmp->next = ptr;
+			if (tmp->prev)
+				ptr = tmp->prev;
+			else
+				*list = tmp;
+			print_list(*list);
+		}
+		ptr = ptr->next;
+	}
+}
